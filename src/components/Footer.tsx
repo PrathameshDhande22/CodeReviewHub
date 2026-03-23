8bde93f7f93ae9bc4db27a176188be728f32a037
Prathameshdhande22 <prathameshdhande534@gmail.com>
2026-03-24 05:26:47 +0530

    Implemented the Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,45 @@
 "use client";
+import { Space_Grotesk } from "next/font/google";
+import Link from "next/link";
+
+//#region Font Declaration
+const space_grotesk = Space_Grotesk({
+  subsets: ["latin"],
+});
+//#endregion
 
 const Footer = () => {
-  return <footer></footer>;
+  return (
+    <footer className={`${space_grotesk.className} dark:bg-neutral flex flex-row flex-wrap justify-between gap-2 p-4 items-center`}>
+      {/* Logo */}
+      <div>
+        <Link href={"/"} className="flex flex-row gap-2">
+          <span className="text-sky-400 font-bold block self-center">
+            CodeReview Hub
+          </span>
+        </Link>
+        <div className="uppercase font-semibold text-neutral-500 md:text-sm text-xs">
+          @ {new Date().getUTCFullYear()} The Code Review Hub. The Digital
+          Architect
+        </div>
+      </div>
+      {/* Footer Nav Links */}
+      <div className="font-semibold text-sm text-neutral-500">
+        <ul className="flex flex-row items-center gap-3">
+          <li>
+            <Link href={"/terms"}>
+              <span>TERMS OF SERVICE</span>
+            </Link>
+          </li>
+          <li>
+            <Link href={"/privacy"}>
+              <span>PRIVACY POLICY</span>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;

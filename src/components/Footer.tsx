b5b201615c01138f32de01e6406eebbbfce739ba
Prathameshdhande22 <prathameshdhande534@gmail.com>
2026-03-24 06:15:47 +0530

    Issue while rendering fixed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Space_Grotesk } from "next/font/google";
 import Link from "next/link";
+import { Suspense } from "react";
 
 //#region Font Declaration
 const space_grotesk = Space_Grotesk({
@@ -8,9 +9,15 @@ const space_grotesk = Space_Grotesk({
 });
 //#endregion
 
+const YearComponent = () => {
+  return <span suppressHydrationWarning>{new Date().getUTCFullYear()}</span>;
+};
+
 const Footer = () => {
   return (
-    <footer className={`${space_grotesk.className} dark:bg-neutral flex flex-row flex-wrap justify-between gap-2 p-4 items-center`}>
+    <footer
+      className={`${space_grotesk.className} dark:bg-neutral flex flex-row flex-wrap justify-between gap-2 p-4 items-center`}
+    >
       {/* Logo */}
       <div>
         <Link href={"/"} className="flex flex-row gap-2">
@@ -19,8 +26,11 @@ const Footer = () => {
           </span>
         </Link>
         <div className="uppercase font-semibold text-neutral-500 md:text-sm text-xs">
-          @ {new Date().getUTCFullYear()} The Code Review Hub. The Digital
-          Architect
+          @{" "}
+          <Suspense fallback={<span>...</span>}>
+            <YearComponent />
+          </Suspense>{" "}
+          The Code Review Hub. The Digital Architect
         </div>
       </div>
       {/* Footer Nav Links */}

0f1960908106a280db10aeddd845550369b2a5d7
Prathameshdhande22 <prathameshdhande534@gmail.com>
2026-04-23 07:10:32 +0530

    Tanstack React Query Installation and Added the Providers

--- src/providers/provider.tsx
+++ src/providers/provider.tsx
@@ -1,12 +1,22 @@
 "use client";
+
 import { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 type ProvidersProps = {
   children: React.ReactNode;
   session?: Session | null;
 };
 
+const queryClient = new QueryClient();
+
 export default function Providers({ children, session }: ProvidersProps) {
-  return <SessionProvider session={session}>{children}</SessionProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider session={session}>{children}</SessionProvider>;
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }

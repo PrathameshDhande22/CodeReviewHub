c94460f276f53c937161852bb71c1b48489ad421
Prathamesh Dhande <prathamesh.d@systenics.com>
2026-09-09 15:59:17 +0530

    feat: Update Docker configuration, enhance environment variables, and improve API health checks

--- src/app/api/language/route.ts
+++ src/app/api/language/route.ts
@@ -1,9 +1,10 @@
 import status from "http-status";
 import type { Languages } from "@generated/prisma/client";
-import { NextResponse } from "next/server";
+import { connection, NextResponse } from "next/server";
 import { getLanguages } from "@/services/language.service";
 
 export async function GET() {
+  await connection();
   const languages: Languages[] = await getLanguages();
   return NextResponse.json<Languages[]>(languages, {
     status: status.OK,
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,13 +1,18 @@
 import { getPosts } from "@/db/postcode.repo";
+import { BASE_URL } from "@/lib/seo";
 import { cacheLife } from "next/cache";
 import { MetadataRoute } from "next";
+import { connection } from "next/server";
 
-const BASE_URL = "https://codereviewhub.prathameshd.com";
-
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+async function getSitemapPosts() {
     "use cache";
     cacheLife("hours");
-    const posts = await getPosts(0, Number.MAX_SAFE_INTEGER);
+    return await getPosts(0, Number.MAX_SAFE_INTEGER);
+}
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+    await connection();
+    const posts = await getSitemapPosts();
 
     const postSiteMaps: MetadataRoute.Sitemap = posts.map((post) => ({
         url: `${BASE_URL}/post/${post.id}`,
@@ -30,17 +35,17 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
             priority: 0.9,
         },
         {
-            url: `${BASE_URL}/legal/terms`,
+            url: `${BASE_URL}/legal/terms-and-conditions`,
             lastModified: new Date(),
             changeFrequency: "monthly",
             priority: 0.3,
         },
         {
-            url: `${BASE_URL}/legal/privacy`,
+            url: `${BASE_URL}/legal/privacy-policy`,
             lastModified: new Date(),
             changeFrequency: "monthly",
             priority: 0.3,
         },
         ...postSiteMaps,
     ];
-}
+}
